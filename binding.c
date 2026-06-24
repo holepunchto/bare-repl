0087b72e7d6ecebfd4818be21748fb497bb07f22
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-06-24 15:47:26 +0200

    Support context isolation (#7)

--- binding.c
+++ binding.c
@@ -8,8 +8,69 @@
 #include <utf.h>
 #include <uv.h>
 
+static void
+bare_repl_finalize_context(js_env_t *env, void *data, void *finalize_hint) {
+  int err;
+
+  js_context_t *context = (js_context_t *) data;
+
+  err = js_destroy_context(env, context);
+  assert(err == 0);
+}
+
 static js_value_t *
-bare_repl_eval (js_env_t *env, js_callback_info_t *info) {
+bare_repl_create_context(js_env_t *env, js_callback_info_t *info) {
+  int err;
+
+  js_value_t *argv[1];
+  size_t argc = 1;
+
+  err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
+  assert(err == 0);
+
+  assert(argc == 1);
+
+  js_context_t *context;
+  err = js_create_context(env, &context);
+  assert(err == 0);
+
+  err = js_wrap(env, argv[0], (void *) context, bare_repl_finalize_context, NULL, NULL);
+  assert(err == 0);
+
+  return NULL;
+}
+
+static js_value_t *
+bare_repl_global(js_env_t *env, js_callback_info_t *info) {
+  int err;
+
+  js_value_t *argv[1];
+  size_t argc = 1;
+
+  err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
+  assert(err == 0);
+
+  assert(argc == 1);
+
+  js_context_t *context;
+  err = js_unwrap(env, argv[0], (void **) &context);
+  assert(err == 0);
+
+  err = js_enter_context(env, context);
+  assert(err == 0);
+
+  js_value_t *global;
+  err = js_get_global(env, &global);
+  assert(err == 0);
+
+  err = js_exit_context(env, context);
+  assert(err == 0);
+
+  return global;
+}
+
+static js_value_t *
+bare_repl_eval(js_env_t *env, js_callback_info_t *info) {
   int err;
 
   js_value_t *argv[2];
@@ -34,21 +95,52 @@ bare_repl_eval (js_env_t *env, js_callback_info_t *info) {
 
   free(expr);
 
-  js_value_t *result;
-  err = js_run_script(env, "<repl>", -1, 0, script, &result);
+  // A `null` context evaluates in the current context, i.e. against the shared
+  // global, matching the `useGlobal` option on the JavaScript side.
+  bool use_global;
+  err = js_is_null(env, argv[1], &use_global);
+  assert(err == 0);
 
-  return err == 0 ? result : NULL;
+  js_context_t *context;
+
+  if (!use_global) {
+    err = js_unwrap(env, argv[1], (void **) &context);
+    assert(err == 0);
+
+    err = js_enter_context(env, context);
+    assert(err == 0);
+  }
+
+  js_value_t *result;
+  int script_err = js_run_script(env, "<repl>", -1, 0, script, &result);
+
+  if (!use_global) {
+    err = js_exit_context(env, context);
+    assert(err == 0);
+  }
+
+  return script_err == 0 ? result : NULL;
 }
 
 static js_value_t *
-bare_repl_exports (js_env_t *env, js_value_t *exports) {
-  {
-    js_value_t *fn;
-    js_create_function(env, "eval", -1, bare_repl_eval, NULL, &fn);
-    js_set_named_property(env, exports, "eval", fn);
+bare_repl_exports(js_env_t *env, js_value_t *exports) {
+  int err;
+
+#define V(name, fn) \
+  { \
+    js_value_t *val; \
+    err = js_create_function(env, name, -1, fn, NULL, &val); \
+    assert(err == 0); \
+    err = js_set_named_property(env, exports, name, val); \
+    assert(err == 0); \
   }
+
+  V("createContext", bare_repl_create_context)
+  V("global", bare_repl_global)
+  V("eval", bare_repl_eval)
+#undef V
 
   return exports;
 }
 
-BARE_MODULE(bare_repl, bare_repl_exports);
+BARE_MODULE(bare_repl, bare_repl_exports)
